5b7529158fc68171a7ffe017fdc1408ebd3b6ff3
AK-CCM <33413085+AK-CCM@users.noreply.github.com>
2025-04-26 20:40:07 +0200

    Update mod_inaturalist_observations.php

--- mod_inaturalist_observations/mod_inaturalist_observations.php
+++ mod_inaturalist_observations/mod_inaturalist_observations.php
@@ -2,5 +2,6 @@
 defined('_JEXEC') or die;
 require_once __DIR__ . '/helper.php';
 use Joomla\CMS\Helper\ModuleHelper;
-$observations = ModINatHelper::getObservations($params);
+$data = ModINatHelper::getData($params);
+$avatar = $data['avatar'] ?? '';
 require ModuleHelper::getLayoutPath('mod_inaturalist_observations', $params->get('layout', 'default'));
