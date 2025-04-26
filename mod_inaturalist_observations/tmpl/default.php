038e9d19cb395067853f13e59a5aa174c8689454
AK-CCM <33413085+AK-CCM@users.noreply.github.com>
2025-04-26 02:25:25 +0200

    Update default.php

--- mod_inaturalist_observations/tmpl/default.php
+++ mod_inaturalist_observations/tmpl/default.php
@@ -1,21 +1,26 @@
-<div class="inat-observations" style="display: flex; flex-wrap: wrap; gap: 1rem;">
-<?php if (!empty($observations)): ?>
+<?php
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Layout\FileLayout;
+
+if (empty($observations)) {
+    echo '<p>' . JText::_('MOD_INATURALIST_OBSERVATIONS_NO_RESULTS') . '</p>';
+    return;
+}
+?>
+
+<ul class="inat-observations">
     <?php foreach ($observations as $obs): ?>
-        <div style="width: 180px; font-family: sans-serif;">
-            <?php if (!empty($obs['photos'])): ?>
-                <a href="<?php echo $obs['uri']; ?>" target="_blank" rel="noopener">
-                    <img src="<?php echo str_replace('square', 'medium', $obs['photos'][0]['url']); ?>"
-                         alt="<?php echo htmlspecialchars($obs['taxon']['preferred_common_name'] ?? $obs['taxon']['name']); ?>"
-                         style="width: 100%; border-radius: 8px;">
-                </a>
+        <li class="inat-observation">
+            <?php if (!empty($obs['photos'][0]['url'])): ?>
+                <img src="<?php echo htmlspecialchars(str_replace('square', 'medium', $obs['photos'][0]['url'])); ?>" alt="">
             <?php endif; ?>
-            <p style="margin: 0.5em 0 0; font-size: 0.95em;">
-                <strong><?php echo htmlspecialchars($obs['taxon']['preferred_common_name'] ?? $obs['taxon']['name']); ?></strong><br>
-                <small><?php echo date('d.m.Y', strtotime($obs['observed_on'])); ?></small>
+            <p>
+                <strong><?php echo htmlspecialchars($obs['species_guess'] ?? $obs['taxon']['name'] ?? ''); ?></strong><br>
+                <a href="<?php echo htmlspecialchars($obs['uri']); ?>" target="_blank" rel="noopener">
+                    <?php echo JText::_('MOD_INATURALIST_OBSERVATIONS_VIEW_ON_INATURALIST'); ?>
+                </a>
             </p>
-        </div>
+        </li>
     <?php endforeach; ?>
-<?php else: ?>
-    <p>No observations found.</p>
-<?php endif; ?>
-</div>
+</ul>
